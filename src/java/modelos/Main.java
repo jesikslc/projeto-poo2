8d5896767915aeb65bfadedbf8baf50234241e1c
Fabiana <W10@DESKTOP-4JLCORC>
2019-04-10 19:36:35 -0300

    Classe com método main criando clientes e categorias.

---
+++ src/java/modelos/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import services.CategoriaService;
+import services.ClienteService;
+
+/**
+ *
+ * @author W10
+ */
+public class Main {
+    
+    public static void main(String args[]){
+    
+    CategoriaService servicoCat = new CategoriaService();
+    ClienteService servicoCli = new ClienteService();
+   
+    Categoria cat1 = new Categoria("Infantil");
+    Categoria cat2 = new Categoria("Alimentação");
+    Categoria cat3 = new Categoria("Música");
+            
+    Cliente cli1 = new Cliente(1, 10, "Fabiana Masini", "Rua de teste, 123 - Vila teste",
+        "15667774386", 1200);
+    Cliente cli2 = new Cliente(2, 20, "Thiago Vieira", "Rua de teste 2, 345 - Vila teste 2",
+        "14556664378", 1300);
+    Cliente cli3 = new Cliente(3, 30, "Jéssica", "Rua de teste 3, 456 - Vila teste 3",
+        "12556664732", 1000);
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("Projeto_POO2PU");       
+    
+        servicoCat.salvar(cat1);
+        servicoCat.salvar(cat2);
+        servicoCat.salvar(cat3);
+        
+        servicoCli.salvar(cli1);
+        servicoCli.salvar(cli2);
+        servicoCli.salvar(cli3);
+    }
+    
+}
